f15af3f900237591d7cc74939430c34c8486d7d0
Manu Phatak <bionikspoon@gmail.com>
2016-02-02 22:36:32 -0600

    scaffold app

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,36 @@
-import './main.css';
-
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './components/App';
+import {createStore, compose} from 'redux';
+import {persistState} from 'redux-devtools';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import DevTools from './components/DevTools';
+import App from './components/App';
+import reducer from './lib/reducer';
+
+import './main.scss';
+
+
+const enhancer = compose(
+    DevTools.instrument(), persistState(getDebugSessionKey())
+);
+
+function getDebugSessionKey() {
+    const matches = window.location.href.match(/[?&]debug_session=([^&]+)\b/);
+    return (matches && matches.length > 0 ) ? matches[1] : null;
+}
+
+const store = createStore(reducer, null, enhancer);
+
+
+const provider = (
+    <Provider store={store}>
+        <div>
+            <App />
+
+            <DevTools />
+        </div>
+
+    </Provider>
+);
+ReactDOM.render(provider, document.getElementById('app'));
--- make-webpack-config.js
+++ make-webpack-config.js
@@ -2,13 +2,20 @@ const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const pkg = require('./package.json');
+
 const PATHS = {
   app: path.join(__dirname, 'app'),
+  index: path.join(__dirname, 'app', 'index.tsx'),
   build: path.join(__dirname, 'build')
 };
 
 var commonConfig = {
-  entry: path.join(PATHS.app, 'index.tsx'),
+  entry: PATHS.index,
   resolve: {
     extensions: [
       '',
@@ -19,30 +26,36 @@ var commonConfig = {
   },
   output: {
     path: PATHS.build,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: [
-          'style',
-          'css'
-        ],
-        include: PATHS.app
-      },
+
       {
         test: /\.tsx?$/,
         loader: 'ts-loader',
         include: PATHS.app
       }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.ejs',
+      title: '2048',
+      appMountId: 'app',
+      inject: false
+    })
+  ],
+  postcss: function() {
+    return [
+      require('autoprefixer'),
+      require('precss')
+    ]
   }
 };
 
 var devConfig = {
   devServer: {
-    contentBase: PATHS.build,
     historyApiFallback: true,
     hot: true,
     inline: true,
@@ -55,13 +68,67 @@ var devConfig = {
     host: process.env.HOST,
     port: process.env.PORT
   },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          'css',
+          'postcss',
+          'sass'
+        ],
+        include: PATHS.app
+      }
+    ]
+  },
   plugins: [
     new webpack.HotModuleReplacementPlugin()
   ],
   devTool: 'eval-source-map'
 };
 
-var prodConfig = {};
+var prodConfig = {
+  entry: {
+    app: PATHS.index,
+    vendor: Object.keys(pkg.dependencies)
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[chunkhash].js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css', 'postcss', 'sass'),
+        include: PATHS.app
+      }
+    ]
+  },
+  plugins: [
+    new CleanPlugin([PATHS.build], {
+      verbose: false
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new ExtractTextPlugin('styles.[chunkhash].css'),
+    new webpack.optimize.CommonsChunkPlugin({
+      names: [
+        'vendor',
+        'manifest'
+      ]
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('productions')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ]
+};
 
 module.exports = function(env) {
   switch(env) {
