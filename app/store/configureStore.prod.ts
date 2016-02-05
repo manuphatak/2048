9f0f97c586e736b1c639d3cb85f2375e5328e35b
Manu Phatak <bionikspoon@gmail.com>
2016-02-04 20:08:44 -0600

    initial commit

--- app/store/configureStore.prod.ts
+++
@@ -1,8 +0,0 @@
-import {Map} from 'immutable';
-import {createStore} from 'redux';
-import reducer from '../lib/reducer';
-
-
-export default function configureStoreProduction(initialState = Map()) {
-    return createStore(reducer, initialState);
-}
--- tests.webpack.js
+++
@@ -1,5 +0,0 @@
-var PATHS = require('./webpack.utils').PATHS;
-var context = require.context(PATHS.app, true, /\.spec\.jsx?$/);
-context.keys().forEach(context);
-
-module.exports = context;
